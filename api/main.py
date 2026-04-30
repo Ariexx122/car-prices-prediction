d79521b6f9edbb36242e784e8acffa1c76144213
JordanX101 <hernjordan10@gmail.com>
2026-04-30 14:55:59 -0600

    feat: initial project structure with api and notebooks

---
+++ api/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from catboost import CatBoostRegressor
+from pydantic import BaseModel
+import pandas as pd
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_headers=['*'],
+    allow_methods=['*'],
+    allow_credentials=True
+)
+
+
+class ReviewInput(BaseModel):
+    vehicle_type: str
+    gearbox: str
+    power: int
+    model: str
+    mileage: int
+    registration_month: int
+    fuel_type: str
+    brand: str
+    not_repaired: str
+    age_at_listing: int
+    month_created: int
+
+
+model = CatBoostRegressor()
+model.load_model('cat_model.cbm')
+
+
+@app.post('/predict/')
+def predict(review_input: ReviewInput):
+    df = review_input.model_dump()
+    df = pd.DataFrame([df])
+
+    prediction = model.predict(df)
+
+    return {'prediction': f"{prediction[0]:.2f}"}
